1602d4391da63b90f5a9b4bcdd860adfe8face9b
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-08-29 17:27:57 -0400

    Adding models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :users do
+    resources :expenses
+    resources :groups
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
